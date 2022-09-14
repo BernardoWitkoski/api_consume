6c6e31d686dc2af95e4b19758dc30ece45050254
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-09-14 19:54:06 -0300

    Importing http and convert

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
